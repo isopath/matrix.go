64c1f44aa43fa28d427f7120a4f8810ba191e7b7
x0prc <c0ldheat@protonmail.com>
2026-01-25 01:16:52 +0530

    feat: add macOS support
    style: add pre-commit

--- Makefile
+++ Makefile
@@ -2,14 +2,29 @@ BIN := bin
 TARGET := $(BIN)/asmatrix
 OBJ := $(BIN)/asmatrix.o
 
+# Detect operating system
+UNAME_S := $(shell uname -s)
+
+ifeq ($(UNAME_S),Darwin)
+    # macOS settings
+    ASM_FORMAT := macho64
+    ASM_FLAGS := --prefix _ -DMACOS
+    LDFLAGS := -arch x86_64
+else
+    # Linux settings
+    ASM_FORMAT := elf64
+    ASM_FLAGS :=
+    LDFLAGS := -no-pie
+endif
+
 all: $(TARGET)
 
 $(TARGET): $(OBJ)
-	gcc -no-pie $(OBJ) -o $(TARGET) -lncurses
+	gcc $(LDFLAGS) $(OBJ) -o $(TARGET) -lncurses
 
 $(OBJ): asmatrix.asm
 	mkdir -p $(BIN)
-	nasm -f elf64 asmatrix.asm -o $(OBJ)
+	nasm -f $(ASM_FORMAT) $(ASM_FLAGS) asmatrix.asm -o $(OBJ)
 
 clean:
 	rm -rf $(BIN)
--- asmatrix.asm
+++ asmatrix.asm
@@ -1,5 +1,6 @@
 global main
 
+; imports
 extern initscr
 extern cbreak
 extern noecho
@@ -11,29 +12,45 @@ extern keypad
 extern stdscr
 
 section .data
+    ; msg = ["a", "s", "s", "\n" (10), "\0" (0)]
     msg db "ass", 10, 0
 
 section .text
-main:
-    sub rsp, 8                  ; stack alignment
+default rel
 
+main:
+    ; stack alignment
+    sub rsp, 8
+
+    ; initialise the screen
     call initscr
     call cbreak
     call noecho
 
-    ; keypad(stdscr, TRUE)
-    mov rdi, [rel stdscr]       ; WINDOW *win
-    mov esi, 1                  ; TRUE
+    ; keypad (stdscr, TRUE)
+%ifdef MACOS
+    mov rdi, [stdscr wrt ..gotpcrel]
+    mov rdi, [rdi]
+%else
+    mov rdi, [stdscr]
+%endif
+    mov esi, 1
     call keypad
 
-    lea rdi, [rel msg]
-    xor eax, eax                ; variadic ABI rule
-    call printw
+    ; load address of msg
+    lea rdi, [msg]
 
+    ; since printw is a variadic function,
+    ; we must declare number of registers used.
+    ; xor eax, eax := 0 since we are using
+    ; printw with 0 arguments
+    xor eax, eax
+    call printw
     call refresh
-    call getch                  ; blocks
+    call getch
     call endwin
 
+    ; restore stack pointer
     add rsp, 8
     xor eax, eax
     ret
